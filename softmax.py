8f2be9cda940b3b5977ba346caadcdc34ab321fe
Yamane Sasuke <is0240he@ed.ritsumei.ac.jp>
2016-05-31 20:20:34 +0900

    バッチ処理に変更しscore関数を追加

--- softmax.py
+++ softmax.py
@@ -10,63 +10,59 @@ from sklearn import datasets
 
 
 def softmax(z):
-    return np.exp(z) / np.sum(np.exp(z))
+    return np.exp(z) / np.exp(z).sum(1, keepdims=True)
 
 
 def onehot(k, num_classes=10):
-    t_onehot = np.zeros(num_classes)
-    t_onehot[k] = 1
+    t_onehot = np.zeros((len(k), num_classes))
+    for i, k_i in enumerate(k):
+        t_onehot[i][k_i] = 1
     return t_onehot
 
-# main文
+
+def score(X, t, w):
+    # 正解クラスと予測クラスとの比較
+    y = softmax(np.inner(X, w))
+    predict_class = np.argmax(y, axis=1)
+    return np.mean(t == predict_class)
+
+
 if __name__ == '__main__':
-
-    # Load the digits dataset
-    digits = datasets.load_digits()
-
     # データ・セットの読み込み
+    digits = datasets.load_digits()
     X_raw = digits.data / 16.0
     t = digits.target
     num_examples = len(X_raw)
     X = np.hstack((X_raw, np.ones((num_examples, 1))))
-    # ρを定義する(ρ=0.1で良いか判断し，収束しなければ値を変える．)
-    rho = 0.5
+    num_classes = len(np.unique(t))
+    num_features = X.shape[1]
 
-    # 収束するまで繰り返す
+    # 超パラメータ
     max_iteration = 1000
+    batch_size = 100
+    rho = 0.5  # 学習率
 
-    # dim_features次元の重みをnum_classesクラス分用意する
-    w = np.random.randn(len(np.unique(t)), X.shape[1])
+    # num_features次元の重みをnum_classesクラス分用意する
+    w = np.random.randn(num_classes, num_features)
 
-    # 確率的勾配降下法
-    error_history = []
-    correct_percent_history = []
-
-    # 正解クラスと予測クラスとの比較
-    y = softmax(np.inner(X, w))
-    predict_class = np.argmax(y, axis=1)
-    num_correct = np.sum(t == predict_class)
-    correct_percent = num_correct / float(num_examples) * 100
-    print "correct_percent:", correct_percent
-    correct_percent_history.append(correct_percent)
-
+    num_batches = num_examples / batch_size
     for epoch in range(max_iteration):
         # 入力データXと正解ラベルを取り出す
-        for x_i, t_i in zip(X, t):
+        permu = np.random.permutation(num_examples)
+        for indexes in np.array_split(permu, num_batches):
+            x_batch = X[indexes]
+            t_batch = t[indexes]
             # softmax関数を計算
-            y_i = softmax(np.dot(x_i, w.T))
+            assert(not np.any(np.isnan(w)))
+            y_batch = softmax(np.dot(x_batch, w.T))
+            assert(not np.any(np.isnan(y_batch)))
             # one-hotを適用
-            T = onehot(t_i)
+            T = onehot(t_batch)
             # 勾配降下法
-            w -= rho * np.expand_dims(y_i - T, 1) * x_i
+            w -= rho * np.dot((y_batch - T).T, x_batch)
 
-        # 正解クラスと予測クラスとの比較
-        y = softmax(np.dot(X, w.T))
-        predict_class = np.argmax(y, axis=1)
-        num_correct = np.sum(t == predict_class)
-        correct_percent = num_correct / float(num_examples) * 100
-        print "correct_percent:", correct_percent
-        correct_percent_history.append(correct_percent)
+        correct_rate = score(X, t, w)
+        print "epoch:", epoch, "correct:", correct_rate
 
-        if correct_percent == 100.0:
+        if correct_rate == 1.0:
             break
