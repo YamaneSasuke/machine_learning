ef266c4a9fb2cce6645dd359b46a484881c48783
Yamane Sasuke <is0240he@ed.ritsumei.ac.jp>
2016-06-29 14:33:26 +0900

    Numpy版と同様のコードに修正

--- chainer_neural_network.py
+++ chainer_neural_network.py
@@ -12,18 +12,17 @@ import load_mnist
 from sklearn.cross_validation import train_test_split
 import matplotlib.pyplot as plt
 import time
+import copy
 
 
 # 順伝播の計算
-def forward(model, X, T, train=True):
+def loss_and_accuracy(model, X, T, train=False):
     x = Variable(X)
     t = Variable(T)
-    h1 = F.dropout(F.relu(model.l1(x)), train=train)
-    h2 = F.dropout(F.relu(model.l2(h1)), train=train)
-    y = model.l3(h2)
-
-    return F.softmax_cross_entropy(y, t), F.accuracy(y, t)
-
+    a_z = model.l1(x)
+    z = F.tanh(a_z)
+    a_y = model.l2(z)
+    return F.softmax_cross_entropy(a_y, t), F.accuracy(a_y, t)
 
 if __name__ == '__main__':
     x_train, t_train, x_test, t_test = load_mnist.load_mnist()
@@ -50,8 +49,7 @@ if __name__ == '__main__':
     dim_hidden = 400  # 隠れ層の次元数
     # modelの定義
     model = FunctionSet(l1=F.Linear(num_features, dim_hidden),
-                        l2=F.Linear(dim_hidden, dim_hidden),
-                        l3=F.Linear(dim_hidden, num_classes))
+                        l2=F.Linear(dim_hidden, num_classes))
 
     # Optimizerの設定
     optimizer = optimizers.MomentumSGD(lr=0.01, momentum=0.9)  # Momentum法
@@ -77,19 +75,21 @@ if __name__ == '__main__':
             # 勾配を初期化
             optimizer.zero_grads()
             # 順伝播を計算し、誤差と精度を取得
-            loss, accuracy = forward(model, x_batch, t_batch)
+            loss, accuracy = loss_and_accuracy(model, x_batch, t_batch)
             # 逆伝搬を計算
             loss.backward()
             optimizer.weight_decay(0.001)  # L2正則化を実行
             optimizer.update()
 
         time_end = time.time()
-        loss_train, accuracy_train = forward(model, X_train, T_train)
-        loss_valid, accuracy_valid = forward(model, X_valid, T_valid)
+        loss_train, accuracy_train = loss_and_accuracy(model, X_train, T_train)
+        loss_valid, accuracy_valid = loss_and_accuracy(model, X_valid, T_valid)
 
         if accuracy_valid.data > accuracy_best:
             accuracy_best = accuracy_valid.data
             epoch_best = epoch
+            model_best = copy.deepcopy(model)  # 最善のモデルを確保
+
         accuracy_trains.append(accuracy_train.data)
         accuracy_valids.append(accuracy_valid.data)
         loss_trains.append(loss_train.data)
@@ -121,7 +121,7 @@ if __name__ == '__main__':
         plt.show()
 
     # テストデータの結果を表示
-    loss_test, accuracy_test = forward(model, x_test, t_test)
+    loss_test, accuracy_test = loss_and_accuracy(model_best, x_test, t_test)
     print "[test] accuracy:", accuracy_test.data
     print "[test] loss:", loss_test.data
     print "max_iteration:", max_iteration
